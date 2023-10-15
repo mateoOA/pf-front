291cc239fecf5cb44bef4fa0cb8197a0f07830ba
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-15 20:06:54 -0300

    ready conection

--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -1,19 +1,11 @@
-<<<<<<< Updated upstream
-import NavBar from "../../components/navBar/navBar";
-=======
 import Filtered from "../../components/Filtered/Filtered";
 import { Cards } from "../../components/cards/cards";
->>>>>>> Stashed changes
 
 export default function Products() {
     return (
         <div>
-<<<<<<< Updated upstream
-            <h1>Products</h1>
-=======
             <Filtered />
             <Cards/>
->>>>>>> Stashed changes
         </div>
     )
 }
